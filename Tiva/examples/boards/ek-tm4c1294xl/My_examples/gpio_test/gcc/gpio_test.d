e4c56dbe0213a762c4ba46fada1629563ee9c2f1
Guillermo Auza <guillermo.auza@ucb.edu.bo>
2024-09-06 01:28:36 -0400

    Testing GPIO Functionality

---
+++ Tiva/examples/boards/ek-tm4c1294xl/My_examples/gpio_test/gcc/gpio_test.d
@@ -0,0 +1,6 @@
+gcc/gpio_test.o: gpio_test.c \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ ../../../../../inc/hw_memmap.h ../../../../../driverlib/debug.h \
+ ../../../../../driverlib/gpio.h ../../../../../driverlib/sysctl.h \
+ ../../../../../driverlib/timer.h
---
+++ Tiva/examples/boards/ek-tm4c1294xl/My_examples/gpio_test/gpio_test.c
@@ -0,0 +1,106 @@
+/* 
+Guillermo Javier Auza Banegas
+Embedded systems II
+IMT UCB 2024 S2
+*/
+
+// Libraries
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "driverlib/debug.h"
+#include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/timer.h"
+
+//Macros
+#define CLOCK 120000000
+#define NLEDS (GPIO_PIN_0 | GPIO_PIN_1)
+#define FLEDS (GPIO_PIN_0 | GPIO_PIN_4)
+
+//GPIOPinWrite(uint32_t ui32Port, uint8_t ui8Pins, uint8_t ui8Val)
+//#define GPIO_PIN_0              0x00000001 
+
+/*
+#if defined(codered) || defined(gcc) || defined(sourcerygxx)
+void __attribute__((naked))
+SysCtlDelay(uint32_t ui32Count)
+{
+    __asm("    subs    r0, #1\n"
+          "    bne     SysCtlDelay\n"
+          "    bx      lr");
+}
+*/
+
+/*SysCtlPeripheralEnable(uint32_t ui32Peripheral)
+{
+    //
+    // Check the arguments.
+    //
+    ASSERT(_SysCtlPeripheralValid(ui32Peripheral));
+
+    //
+    // Enable this peripheral.
+    //
+    HWREGBITW(SYSCTL_RCGCBASE + ((ui32Peripheral & 0xff00) >> 8),
+              ui32Peripheral & 0xff) = 1;
+}*/
+
+//#define SYSCTL_PERIPH_GPION     0xf000080c  // GPIO N
+
+//#define GPIO_PORTN_BASE         0x40064000  // GPIO Port N
+//{ GPIO_PORTN_BASE, INT_GPION_TM4C123 },
+
+// 0b binary constants arent available in gcc compiler O_O
+
+void Delay(uint32_t);
+void peripheralStartup(void);
+
+#ifdef DEBUG
+void
+__error__(char *pcFilename, uint32_t ui32Line)
+{
+    while(1);
+}
+#endif
+
+int
+main(void)
+{
+    SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480), CLOCK);
+    peripheralStartup();
+    GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_0);
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_0);
+    GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_1);
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_4);
+    while(1)
+    {
+        GPIOPinWrite(GPIO_PORTN_BASE, NLEDS, GPIO_PIN_1);
+        Delay(500);
+        GPIOPinWrite(GPIO_PORTN_BASE, NLEDS, GPIO_PIN_0 | GPIO_INT_PIN_1);
+        Delay(500);
+        GPIOPinWrite(GPIO_PORTF_BASE, FLEDS, GPIO_PIN_4);
+        Delay(500);
+        GPIOPinWrite(GPIO_PORTF_BASE, FLEDS, GPIO_PIN_0 | GPIO_INT_PIN_4);
+        Delay(500);
+        //GPIOPinWrite(GPIO_PORTF_BASE, FLEDS, !GPIO_PIN_1);
+        Delay(500);
+        //GPIOPinWrite(GPIO_PORTF_BASE, FLEDS, 0x00);
+        Delay(500);
+        //GPIOPinWrite(GPIO_PORTN_BASE, NLEDS, GPIO_PIN_1);
+        Delay(500);
+        //GPIOPinWrite(GPIO_PORTN_BASE, NLEDS, 0x00);
+        Delay(500);
+    }
+}
+
+void Delay(uint32_t time){
+	SysCtlDelay((CLOCK/3000) * time);
+}
+
+void peripheralStartup(void){
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPION)){;}
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF)){;}
+}
