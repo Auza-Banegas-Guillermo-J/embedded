9aeb00bc927e04cb49cd6c3c4bfe62df31201465
Auza-Banegas-Guillermo-J <142273820+Auza-Banegas-Guillermo-J@users.noreply.github.com>
2024-09-10 18:38:10 -0400

    Update Blink_Led.py

--- Rasp/laboratorio_2/Blink_Led.py
+++ Rasp/laboratorio_2/Blink_Led.py
@@ -1,29 +1,21 @@
 import RPi.GPIO as GPIO
 import time
 
-# Set up GPIO mode
 GPIO.setmode(GPIO.BCM)
-
-# Define the GPIO pin you are using
 LED_PIN = 17
-
-# Set up the GPIO pin as an output
 GPIO.setup(LED_PIN, GPIO.OUT)
 
 try:
     while True:
-        # Turn on the LED
         GPIO.output(LED_PIN, GPIO.HIGH)
         print("LED ON")
-        time.sleep(1)  # Wait for 1 second
+        time.sleep(1)
 
-        # Turn off the LED
         GPIO.output(LED_PIN, GPIO.LOW)
         print("LED OFF")
-        time.sleep(1)  # Wait for 1 second
+        time.sleep(1)
 
 except KeyboardInterrupt:
-    # Clean up GPIO on exit
     GPIO.cleanup()
     print("Program terminated")
 
